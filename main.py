86f404219431077bcc42738638209030918a4f2d
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 09:00:37 +0500

    Update bot greeting message and startup log

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import os  
-from telegram import Update  
-from telegram.ext import Application, CommandHandler, ContextTypes  
-  
-TOKEN = os.getenv("BOT_TOKEN")  
-  
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  
-    await update.message.reply_text("🎌 Bot ishlayapti!")  
-  
-def main():  
-    app = Application.builder().token(TOKEN).build()  
-  
-    app.add_handler(CommandHandler("start", start))  
-  
-    print("Bot started...")  
-    app.run_polling()  
-  
-if __name__ == "__main__":  
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 Salom! Bot ishlayapti")
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot ishga tushdi")
+    app.run_polling()
+
+if __name__ == "__main__":
     main()
