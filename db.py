8fabd128a3cbd822c7c28b7ffb3d1c21d15f477f
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 22:20:52 +0500

    Add async functions to get user and add coins

--- db.py
+++ db.py
@@ -6,3 +6,12 @@ db = client.mafia
 
 users = db.users
 games = db.games
+async def get_user(uid):
+    u = await users.find_one({"_id": uid})
+    if not u:
+        u = {"_id": uid, "coins": 0}
+        await users.insert_one(u)
+    return u
+
+async def add_coins(uid, amount):
+    await users.update_one({"_id": uid}, {"$inc": {"coins": amount}})
