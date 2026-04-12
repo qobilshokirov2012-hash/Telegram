9a6b0c95550aea08f4e749d7d895f610c455ddfe
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 08:10:52 +0500

    Refactor bot initialization and start command

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
 from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram import Update
-from pymongo import MongoClient
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-MONGO = os.getenv("MONGO_URL")
 
-# MongoDB
-client = MongoClient(MONGO)
-db = client["anime_bot"]
-users = db["users"]
-
-# START COMMAND
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        user = update.effective_user
+    await update.message.reply_text("Bot ishlayapti 🚀")
 
-        users.update_one(
-            {"user_id": user.id},
-            {"$set": {"name": user.first_name}},
-            upsert=True
-        )
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
-        await update.message.reply_text("🎌 Bot ishlayapti!")
-
-    except Exception as e:
-        await update.message.reply_text(f"Error: {e}")
-
-# MAIN BOT
-def main():
-    app = Application.builder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-
-    print("Bot ishga tushdi...")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+app.run_polling()
