204e75d5d2c25388c79e7c4a4071c59bbdc36a97
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 07:37:55 +0500

    Update user information in MongoDB

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-from pymongo import MongoClient
-
-MONGO = os.getenv("MONGO_URL")
-client = MongoClient(MONGO)
-
-db = client["anime_bot"]
-users = db["users"]
+users.update_one(
+    {"user_id": update.effective_user.id},
+    {"$set": {"name": update.effective_user.first_name}},
+    upsert=True
+)
