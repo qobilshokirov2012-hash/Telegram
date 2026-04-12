6c75218ce7d5d1c7107f0a55c0ad96776dd734b0
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 22:31:08 +0500

    Add buy_item function for purchasing items

--- handlers/shop.py
+++ handlers/shop.py
@@ -4,3 +4,19 @@ SHOP = {
     "gun": 60,
     "fake_doc": 70
 }
+from db import users, add_coins
+
+async def buy_item(uid, item, price):
+    user = await users.find_one({"_id": uid})
+
+    if user["coins"] < price:
+        return False
+
+    await users.update_one(
+        {"_id": uid},
+        {
+            "$inc": {"coins": -price},
+            "$inc": {f"inventory.{item}": 1}
+        }
+    )
+    return True
