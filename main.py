2f406e53ca075e02910b05013805e8a56c45ec11
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 08:35:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import os
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram import Update
-
-TOKEN = os.getenv("BOT_TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🎌 Bot ishlayapti!")
-
-def main():
-    app = Application.builder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-
-    print("Bot started...")
-    app.run_polling()
-
-if __name__ == "__main__":
+import os  
+from telegram import Update  
+from telegram.ext import Application, CommandHandler, ContextTypes  
+  
+TOKEN = os.getenv("BOT_TOKEN")  
+  
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  
+    await update.message.reply_text("🎌 Bot ishlayapti!")  
+  
+def main():  
+    app = Application.builder().token(TOKEN).build()  
+  
+    app.add_handler(CommandHandler("start", start))  
+  
+    print("Bot started...")  
+    app.run_polling()  
+  
+if __name__ == "__main__":  
     main()
