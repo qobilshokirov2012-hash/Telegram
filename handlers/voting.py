31b120ade2f301f95cf9bc13e9b42cb76f2dff48
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-12 22:29:02 +0500

    Add vote calculation function using Counter

--- handlers/voting.py
+++ handlers/voting.py
@@ -12,3 +12,14 @@ async def vote(chat_id, voter, target):
         {"chat_id": chat_id},
         {"$set": {"votes": game["votes"]}}
     )
+from collections import Counter
+
+async def calculate_votes(game):
+    votes = game.get("votes", {})
+    count = Counter(votes.values())
+
+    if not count:
+        return None
+
+    target = count.most_common(1)[0][0]
+    return int(target)
