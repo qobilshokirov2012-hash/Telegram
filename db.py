ba5e9a86301890b281acc6b6b7f437371f00da11
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-14 19:04:51 +0500

    Refactor db.py to use AsyncIOMotorClient

--- db.py
+++ db.py
@@ -6,3 +6,27 @@ db = client.mafia
 
 users = db.users
 games = db.games
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.mafia
+
+users = db.users
+
+
+# 👤 USER OLISH / YARATISH
+async def get_user(uid):
+    u = await users.find_one({"_id": uid})
+    if not u:
+        u = {"_id": uid, "coins": 0}
+        await users.insert_one(u)
+    return u
+
+
+# 💰 COINS QO‘SHISH
+async def add_coins(uid, amount):
+    await users.update_one(
+        {"_id": uid},
+        {"$inc": {"coins": amount}}
+    )
