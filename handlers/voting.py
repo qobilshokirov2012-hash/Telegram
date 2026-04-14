c570f1bdada9e4319e1b29dc571d5dc4c9bcf1ee
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-14 19:17:20 +0500

    Update voting.py

--- handlers/voting.py
+++ handlers/voting.py
@@ -1,25 +1,8 @@
-from db import games
+from aiogram import Router, F
+from aiogram.types import Message
 
-async def vote(chat_id, voter, target):
-    game = await games.find_one({"chat_id": chat_id})
+router = Router()
 
-    if "votes" not in game:
-        game["votes"] = {}
-
-    game["votes"][str(voter)] = target
-
-    await games.update_one(
-        {"chat_id": chat_id},
-        {"$set": {"votes": game["votes"]}}
-    )
-from collections import Counter
-
-async def calculate_votes(game):
-    votes = game.get("votes", {})
-    count = Counter(votes.values())
-
-    if not count:
-        return None
-
-    target = count.most_common(1)[0][0]
-    return int(target)
+@router.message(F.text == "/vote")
+async def vote(msg: Message):
+    await msg.answer("🗳 Ovoz berish boshlandi")
