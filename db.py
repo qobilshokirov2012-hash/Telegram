3bc860abb059bbf613ab3fc969e9c65d22e91064
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-14 19:00:08 +0500

    Remove user coin management functions
    Removed user-related functions for managing coins and rewards.

--- db.py
+++ db.py
@@ -6,21 +6,3 @@ db = client.mafia
 
 users = db.users
 games = db.games
-async def get_user(uid):
-    u = await users.find_one({"_id": uid})
-    if not u:
-        u = {"_id": uid, "coins": 0}
-        await users.insert_one(u)
-    return u
-
-async def add_coins(uid, amount):
-    await users.update_one({"_id": uid}, {"$inc": {"coins": amount}})
-# game ichiga qo‘shiladi
-# votes: {user_id: target_id}
-async def reward(uid, action):
-    if action == "kill":
-        await add_coins(uid, 10)
-    if action == "win":
-        await add_coins(uid, 50)
-    if action == "vote_win":
-        await add_coins(uid, 5)
