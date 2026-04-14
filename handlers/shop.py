0418df2d174d0789747dac08e84b09176b895430
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-14 19:16:42 +0500

    Refactor shop handling with aiogram integration
    Refactored shop functionality to use aiogram Router and added a new shop command.

--- handlers/shop.py
+++ handlers/shop.py
@@ -1,22 +1,8 @@
-SHOP = {
-    "shield": 40,
-    "anti_kill": 50,
-    "gun": 60,
-    "fake_doc": 70
-}
-from db import users, add_coins
+from aiogram import Router, F
+from aiogram.types import Message
 
-async def buy_item(uid, item, price):
-    user = await users.find_one({"_id": uid})
+router = Router()
 
-    if user["coins"] < price:
-        return False
-
-    await users.update_one(
-        {"_id": uid},
-        {
-            "$inc": {"coins": -price},
-            "$inc": {f"inventory.{item}": 1}
-        }
-    )
-    return True
+@router.message(F.text == "/shop")
+async def shop(msg: Message):
+    await msg.answer("🛒 Shop ochildi")
