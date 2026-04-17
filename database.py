dafa63b2b95bb66e9bb29bbc21eb396e0e8c9c1b
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-17 15:14:27 +0500

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL
-
-client = AsyncIOMotorClient(MONGO_URL)
-db = client.mafia_bot
-
-games = db.games
-users = db.users
