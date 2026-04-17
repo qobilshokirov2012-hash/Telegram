98c65b827c8309e2c40b965d1626d8ed0f7efd3a
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-17 15:14:00 +0500

    Delete handlers directory

--- handlers/shop.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, F
-from aiogram.types import Message
-
-router = Router()
-
-@router.message(F.text == "/shop")
-async def shop(msg: Message):
-    await msg.answer("🛒 Shop ochildi")
--- handlers/voting.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, F
-from aiogram.types import Message
-
-router = Router()
-
-@router.message(F.text == "/vote")
-async def vote(msg: Message):
-    await msg.answer("🗳 Ovoz berish boshlandi")
